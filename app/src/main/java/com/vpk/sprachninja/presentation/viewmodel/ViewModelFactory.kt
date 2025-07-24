2144c5e4a1f0da13951d407958c4e71eaba9ba5e
VISHNUPRASAD KAMALON <devvpk@gmail.com>
2025-07-24 13:20:59 +0200

    Settings Feature - Presentation Layer (#8)
    * Create the Settings ViewModel
    
    * Update the ViewModelFactory for Settings
    
    * Create the Settings UI Screen

--- app/src/main/java/com/vpk/sprachninja/presentation/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/vpk/sprachninja/presentation/viewmodel/ViewModelFactory.kt
@@ -3,9 +3,18 @@ package com.vpk.sprachninja.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.vpk.sprachninja.di.AppContainer
+import com.vpk.sprachninja.domain.usecase.GetSettingsUseCase
 import com.vpk.sprachninja.domain.usecase.GetUserUseCase
+import com.vpk.sprachninja.domain.usecase.SaveSettingsUseCase
 import com.vpk.sprachninja.domain.usecase.SaveUserUseCase
 
+/**
+ * A custom factory for creating ViewModel instances.
+ * This is necessary because our ViewModels have constructors with dependencies
+ * that need to be provided from our manual DI container (AppContainer).
+ *
+ * @property appContainer The application's dependency container.
+ */
 class ViewModelFactory(private val appContainer: AppContainer) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
@@ -19,6 +28,12 @@ class ViewModelFactory(private val appContainer: AppContainer) : ViewModelProvid
             modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
                 HomeViewModel(
                     getUserUseCase = GetUserUseCase(appContainer.userRepository)
+                ) as T
+            }
+            modelClass.isAssignableFrom(SettingsViewModel::class.java) -> {
+                SettingsViewModel(
+                    getSettingsUseCase = GetSettingsUseCase(appContainer.settingsRepository),
+                    saveSettingsUseCase = SaveSettingsUseCase(appContainer.settingsRepository)
                 ) as T
             }
             else -> {
